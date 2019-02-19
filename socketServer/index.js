2f601b5d23fdf44d7a14da92064facb4ebbe0244
hngochanh <hngochanh@outlook.com>
2019-02-19 21:40:00 +0700

    wifi

---
+++ socketServer/index.js
@@ -0,0 +1,26 @@
+const PORT = 3484;
+
+var http = require('http')
+var socketio = require('socket.io')
+
+var ip = require('ip');
+var app = http.createServer();
+var io = socketio(app);
+
+app.listen(PORT);
+console.log("Server nodejs: " + ip.address() + ":" + PORT)
+
+var count = 0;
+
+io.on('connection', function(socket) {	
+	
+    console.log("connected");
+	
+	socket.on("messageType", function(data) {
+		console.log(data);
+	});
+		
+	socket.on('disconnect', function() {
+		console.log("disconnect")
+	})
+});
